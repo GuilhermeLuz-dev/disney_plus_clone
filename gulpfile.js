c35b46305cb1738f6f58225fc4fc8bafd53cb3b0
Guilherme Rocha <luzguilherme.dev@gmail.com>
2024-10-15 09:05:48 -0300

    Criando hero e adicionando pasta de fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
 
 function buildStyles(){
     return gulp.src("./src/styles/*.scss")
@@ -8,9 +9,14 @@ function buildStyles(){
             }))
             .pipe(gulp.dest("dist/styles"));    
 }
+function imageMin(){
+    return gulp.src("./src/images/**/*")
+            .pipe(imagemin())
+            .pipe(gulp.dest("/dist/images"));    
+}
 
 
-exports.default = buildStyles;
+exports.default = gulp.parallel(buildStyles, imageMin);
 
 exports.watch = function(){
     gulp.watch("src/styles/*.scss", gulp.parallel(buildStyles));
