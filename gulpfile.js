dc105672df589880c1ce51569c8765d4e283409e
Guilherme Rocha <luzguilherme.dev@gmail.com>
2024-10-22 17:35:31 -0300

    build

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,31 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
 
-function buildStyles(){
+function scripts(){
+    return gulp.src("./src/scripts/*.js")
+            .pipe(uglify())
+            .pipe(gulp.dest("./dist/js"));
+}
+
+function buildStyles() {
     return gulp.src("./src/styles/*.scss")
-            .pipe(sass({
-                outputStyle: "compressed"
-            }))
-            .pipe(gulp.dest("dist/styles"));    
+        .pipe(sass({
+            outputStyle: "compressed"
+        }))
+        .pipe(gulp.dest("dist/styles"));
 }
-function imageMin(){
+function imageMin() {
     return gulp.src("./src/images/**/*")
-            .pipe(imagemin())
-            .pipe(gulp.dest("/dist/images"));    
+        .pipe(imagemin())
+        .pipe(gulp.dest("/dist/images"));
 }
 
 
-exports.default = gulp.parallel(buildStyles, imageMin);
+exports.default = gulp.parallel(buildStyles, imageMin, scripts);
 
-exports.watch = function(){
+exports.watch = function () {
     gulp.watch("src/styles/*.scss", gulp.parallel(buildStyles));
+    gulp.watch("src/scripts/*.js", gulp.parallel(scripts));
 }
